b7bf78486bc8018cf9c7ec27ca5dd88e116c96cb
Brett <brettgraham@gmail.com>
2024-05-24 13:15:40 -0400

    TST: system info to pytest header

--- pytest_crds/plugin.py
+++ pytest_crds/plugin.py
@@ -5,9 +5,24 @@ def pytest_addoption(parser):
 
 def pytest_report_header(config):
     """Add CRDS_CONTEXT to pytest report header"""
+    lines = []
+
+    import glob
+    fns = glob.glob("/etc/*-release")
+    if fns:
+        lines.append("==== system info ====")
+        for fn in fns:
+            with open(fn, "r") as f:
+                lines.extend(f.readlines())
+        lines.append("== end system info ==")
+
+    import numpy as np
+    lines.append(f"np.double precision    : {np.finfo(np.double).precision}")
+    lines.append(f"np.longdouble precision: {np.finfo(np.longdouble).precision}")
 
     if config.getoption("report_crds_context"):
         from stpipe.crds_client import get_context_used
-        return f"crds_context: {get_context_used('jwst')}"
-    else:
-        return []
+        lines.append(f"crds_context: {get_context_used('jwst')}")
+
+
+    return lines
