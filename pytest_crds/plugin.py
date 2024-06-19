a2058cdc99eddd8f09e89c330c2280e83a7bbe30
Brett <brettgraham@gmail.com>
2024-06-19 09:45:28 -0400

    print out stcal and stdatamodels versions

--- pytest_crds/plugin.py
+++ pytest_crds/plugin.py
@@ -5,9 +5,13 @@ def pytest_addoption(parser):
 
 def pytest_report_header(config):
     """Add CRDS_CONTEXT to pytest report header"""
+    lines = []
 
     if config.getoption("report_crds_context"):
         from stpipe.crds_client import get_context_used
-        return f"crds_context: {get_context_used('jwst')}"
-    else:
-        return []
+        lines.append(f"crds_context: {get_context_used('jwst')}")
+    import stcal
+    lines.append(f"stcal: {stcal.__version__}")
+    import stdatamodels
+    lines.append(f"stdatamodels: {stdatamodels.__version__}")
+    return lines
