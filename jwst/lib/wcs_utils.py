f75c0ea8c91dbfe807ab56155bb05ad005f1f8a3
Brett <brettgraham@gmail.com>
2025-04-29 16:43:50 -0400

    fix hasattr use in wcs_utils

--- jwst/lib/wcs_utils.py
+++ jwst/lib/wcs_utils.py
@@ -33,7 +33,7 @@ def get_wavelengths(model, exp_type="", order=None, use_wavecorr=None):
         An array of wavelengths corresponding to the data in ``model``.
     """
     # Use the existing wavelength array, if there is one
-    if hasattr(model, "wavelength"):
+    if model.hasattr("wavelength"):
         wl_array = model.wavelength.copy()
         got_wavelength = True  # may be reset below
     else:
@@ -65,7 +65,7 @@ def get_wavelengths(model, exp_type="", order=None, use_wavecorr=None):
     if use_wavecorr is not None:
         if (
             not use_wavecorr
-            and hasattr(model.meta, "wcs")
+            and model.meta.hasattr("wcs")
             and "wavecorr_frame" in model.meta.wcs.available_frames
         ):
             wcs = model.meta.wcs
@@ -75,9 +75,9 @@ def get_wavelengths(model, exp_type="", order=None, use_wavecorr=None):
             return wl_array
 
     # If no existing wavelength array, compute one
-    if hasattr(model.meta, "wcs") and not got_wavelength:
+    if model.meta.hasattr("wcs") and not got_wavelength:
         # Set up an appropriate WCS object
-        if hasattr(model.meta, "exposure") and model.meta.exposure.type == "NIS_SOSS":
+        if model.meta.hasattr("exposure") and model.meta.exposure.type == "NIS_SOSS":
             wl_array = model.meta.wcs(grid[1], grid[0], order)[2]
             return wl_array
 
